cee46cec1cef2b6299791dd0b681d66d02eb3f44
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-08-24 19:18:38 +0530

    12. API

---
+++ UserManagementApp/src/server/controller/controller.js
@@ -0,0 +1,21 @@
+var Userdb = require('../model/model');
+
+//create and save new user
+exports.create = (req,res) => {
+
+}
+
+// retrieve and return all users / retrieve and return single users
+exports.find = (req,res) => {
+
+}
+
+// Update a new identified user by UserID
+exports.update = (req,res) => {
+
+}
+
+//Delete a user with specified userId
+exports.delete = (req,res) =>{
+    
+}
