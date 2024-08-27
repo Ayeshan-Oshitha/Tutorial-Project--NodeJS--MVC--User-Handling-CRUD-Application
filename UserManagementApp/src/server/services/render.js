fe49ee4fb014b0c453d7d6cbff5291964f7ffbe7
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-08-27 13:41:00 +0530

    17. Update using API ( not working corrdctly )

--- UserManagementApp/src/server/services/render.js
+++ UserManagementApp/src/server/services/render.js
@@ -16,6 +16,12 @@ exports.add_user = (req,res) => {
     res.render('add_user');
 }
 
-exports.update_user = (req,res) => {
-    res.render('update_user');
+exports.update_user = (req, res) => {
+    axios.get('http://localhost:3000/api/users', { params: { id: req.query.id } })
+        .then(function (response) {
+            res.render("update_user", { user: response.data });
+        })
+        .catch(err => {
+            res.send(err);
+        });
 }
